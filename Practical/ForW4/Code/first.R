a67f0b388aa70bcce744887ce4c578f1387e53b2
Jonnie Carino <jcarino2003@hotmail.com>
2026-02-19 14:24:15 +0000

    Add initial project files including .gitignore, Iris dataset, SQLite database, and R project configuration

---
+++ Practical/ForW4/Code/first.R
@@ -0,0 +1,10 @@
+# 1. Using / Importing iris dataset
+
+# Use built in iris dataset
+df <- iris 
+# Import downloaded iris dataset 
+# df <- read.csv("Data/Iris.csv") # Read csv, file location 
+
+# 
+
+
